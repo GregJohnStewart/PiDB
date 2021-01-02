3e3f310b4c558ea0f45192f16f7ea216309175bc
Greg Stewart <contact@gjstewart.net>
2021-01-01 21:47:04 -0500

    Minor cleanup

--- src/main.c
+++ src/main.c
@@ -9,11 +9,10 @@ int main() {
 
 	puts("Pi:");
 	printf("%d.", piDigit(0));
-	for(int i = 1; i < 50; i++){
+	for(int i = 1; i < 500; i++){
 		printf("%d", piDigit(i));
 	}
-	puts("");
-	puts("Done.");
-//	printf("Hello World %d.%d%d%d%d%d\n", piDigit(0), piDigit(1), piDigit(2), piDigit(3), piDigit(4), piDigit(5));
+
+	puts("\nDone.");
 	return 0;
 }
