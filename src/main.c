65fe486dc620a1c6479cae20eca15d5dc2dd1566
Greg Stewart <contact@gjstewart.net>
2021-02-20 19:46:55 -0500

    Added (commended out) code for future handling of arguments using getopt

--- src/main.c
+++ src/main.c
@@ -6,8 +6,65 @@
 
 #include <dataTypes.h>
 #include <byteFinder.h>
+#include <getopt.h>
+#include <stdlib.h>
 
-int main() {
+//#define USAGE                                                   \
+//  "usage:\n"                                                    \
+//  "  webclient [options]\n"                                     \
+//  "options:\n"                                                  \
+//  "  -h                  Show this help message\n"              \
+//  "  -r [num_requests]   Request download total (Default: 5)\n" \
+//  "  -p [server_port]    Server port (Default: 30605)\n"        \
+//  "  -s [server_addr]    Server address (Default: 127.0.0.1)\n" \
+//  "  -t [nthreads]       Number of threads (Default 8)\n"       \
+//  "  -w [workload_path]  Path to workload file (Default: workload.txt)\n"
+///* OPTIONS DESCRIPTOR ====================================================== */
+//static struct option gLongOptions[] = {
+//		{"help",          no_argument,       NULL, 'h'},
+//		{"nthreads",      required_argument, NULL, 't'},
+//		{"nrequests",     required_argument, NULL, 'r'},
+//		{"server",        required_argument, NULL, 's'},
+//		{"port",          required_argument, NULL, 'p'},
+//		{"workload-path", required_argument, NULL, 'w'},
+//		{NULL,            0,                 NULL, 0}};
+//static void Usage() { fprintf(stderr, "%s", USAGE); }
+
+int main(int argc, char **argv) {
+
+//	int option_char = 0;
+//	while ((option_char = getopt_long(argc, argv, "p:n:hs:w:r:t:", gLongOptions,
+//									  NULL)) != -1) {
+//		switch (option_char) {
+//			case 'h':  // help
+//				Usage();
+//				exit(0);
+//				break;
+//			case 'r':
+//				nrequests = atoi(optarg);
+//				break;
+//			case 'n':  // nrequests
+//				break;
+//			case 'p':  // port
+//				port = atoi(optarg);
+//				break;
+//			default:
+//				Usage();
+//				exit(1);
+//			case 's':  // server
+//				server = optarg;
+//				break;
+//			case 't':  // nthreads
+//				nthreads = atoi(optarg);
+//				break;
+//			case 'w':  // workload-path
+//				workload_path = optarg;
+//				break;
+//		}
+//	}
+
+
+
 	printf("Doing the thing\n");
 
 	puts("Pi:");
